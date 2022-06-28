6a6477babc1543d6425ea8365bd3802bc6e6eac7
auto200 <michal.warac@gmail.com>
2022-06-28 22:00:43 +0200

    first draft of working timer

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,40 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+
+void showLayoutGuidelines() {
+  debugPaintSizeEnabled = true;
+}
 
 void main() {
   runApp(const MyApp());
+  // showLayoutGuidelines();
 }
+
+final MILLISECONDS_IN_HOUR = const Duration(hours: 1).inMilliseconds;
+
+String formatDuration(Duration d) =>
+    d.toString().split('.').first.padLeft(8, "0");
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Time is Money',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Time is Money'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,68 +43,149 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  num _hourlyWage = 1000;
+  num _earnings = 0;
+  DateTime? _lastTickTime;
+  Timer? _timer;
+  Duration _totalElapsedMilliseconds = const Duration();
+  late num _earningsPerMillisecond = _hourlyWage / MILLISECONDS_IN_HOUR;
 
-  void _incrementCounter() {
+  final _formKey = GlobalKey<FormState>();
+
+  void setHourlyWage(num wage) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _hourlyWage = wage;
+      _earningsPerMillisecond = _hourlyWage / MILLISECONDS_IN_HOUR;
+    });
+  }
+
+  void toggleTimer() {
+    if (_timer != null) {
+      _timer?.cancel();
+      setState(() {
+        _timer = null;
+        _lastTickTime = null;
+      });
+      return;
+    }
+    _timer = Timer.periodic(const Duration(milliseconds: 100), (timer) {
+      if (_lastTickTime == null) {
+        _lastTickTime = DateTime.now();
+        return;
+      }
+      setState(() {
+        final elapsedMilliseconds =
+            DateTime.now().difference(_lastTickTime!).inMilliseconds;
+        _earnings += _earningsPerMillisecond * elapsedMilliseconds;
+        _lastTickTime = DateTime.now();
+        _totalElapsedMilliseconds = _totalElapsedMilliseconds +
+            Duration(milliseconds: elapsedMilliseconds);
+      });
     });
   }
 
   @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Tak Ci leci pitos modro:',
             ),
+            Text("Your hourly wage: $_hourlyWage"),
+            Text("Elapsed time: ${formatDuration(_totalElapsedMilliseconds)}"),
             Text(
-              '$_counter',
+              '${_earnings.toStringAsFixed(2)} zł', // TODO: change fixed currency
               style: Theme.of(context).textTheme.headline4,
             ),
+            RawMaterialButton(
+              onPressed: toggleTimer,
+              elevation: 2.0,
+              fillColor: Colors.white,
+              padding: const EdgeInsets.all(15.0),
+              shape: const CircleBorder(),
+              child: Icon(
+                _timer == null ? Icons.play_arrow : Icons.pause,
+                size: 35.0,
+              ),
+            )
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () => showDialog(
+            context: context,
+            builder: (ctx) => AlertDialog(
+                  title: const Text("Set hourly wage"),
+                  content: SingleChildScrollView(
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            initialValue: _hourlyWage.toString(),
+                            keyboardType: TextInputType.number,
+                            inputFormatters: [
+                              FilteringTextInputFormatter.digitsOnly
+                            ],
+                            decoration: const InputDecoration(
+                              hintText: "Hourly wage",
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return "Provide correct value";
+                              }
+                            },
+                            onSaved: (value) {
+                              if (value == null) return;
+                              // TODO: check if there is a better solution
+                              final isProbablyDouble = value.contains(".");
+                              final number = isProbablyDouble
+                                  ? double.tryParse(value)
+                                  : int.tryParse(value);
+                              if (number == null) return;
+                              setHourlyWage(number);
+                            },
+                          ),
+                          DropdownButtonFormField(
+                            value: "PLN",
+                            items: const <String>["PLN", "asdf"]
+                                .map((el) => DropdownMenuItem(
+                                      value: el,
+                                      child: Text(el),
+                                    ))
+                                .toList(),
+                            onChanged: (_) {},
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  actions: <Widget>[
+                    TextButton(
+                        onPressed: () {
+                          if (_formKey.currentState == null) return;
+                          if (!_formKey.currentState!.validate()) return;
+                          _formKey.currentState!.save();
+                          Navigator.of(context).pop();
+                        },
+                        child: const Text("ok"))
+                  ],
+                )),
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        child: const Icon(Icons.settings),
+      ),
     );
   }
 }
